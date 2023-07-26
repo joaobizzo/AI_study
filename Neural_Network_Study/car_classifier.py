a40eef8f9e8ffc75bff7d54314ca8212a45e3067
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-07-25 22:58:07 -0300

    Update car_classifier.py
    add first version

--- Neural_Network_Study/car_classifier.py
+++ Neural_Network_Study/car_classifier.py
@@ -1,8 +1,80 @@
 # python file ofr my fisrt network
 #create a model using neural network to classificate the types of car
-import pandas as pd
-import pytorch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
 
-data = 'dados.csv'
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),  # Resize the images to a fixed size
+    transforms.ToTensor(),        # Convert images to PyTorch tensors
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize the data
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)    # 3 input channels (RGB), 6 output channels, 5x5 kernel
+        self.pool = nn.MaxPool2d(2, 2)    # Max pooling layer with 2x2 kernel
+        self.conv2 = nn.Conv2d(6, 16, 5)   # 6 input channels, 16 output channels, 5x5 kernel
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)   # Fully connected layer
+        self.fc2 = nn.Linear(120, 84)      # Fully connected layer
+        self.fc3 = nn.Linear(84, 10)       # Fully connected layer
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # Flatten the tensor to fit the fully connected layers
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+net.to(device)
+
+for epoch in range(10):  # Adjust the number of epochs as needed
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # Print every 2000 mini-batches
+            print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 2000:.3f}")
+            running_loss = 0.0
+
+print("Training finished.")
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy on test images: {100 * correct / total}%")
 
 
