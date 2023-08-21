08a61653b6e6fc47c337eaaacdaadc7e4ce82f32
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-08-21 19:11:30 -0300

    Add files via upload

--- text-recognizer/main.py
+++ text-recognizer/main.py
@@ -1,53 +1,121 @@
-import cv2
+import pytesseract
 import numpy as np
+import cv2 
+import re
+import os
+import matplotlib.pyplot as plt
 
-img = cv2.imread('image.jpg')
+from PIL import ImageFont, ImageDraw, Image
+from pytesseract import Output
 
-# get grayscale image
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+projeto = "imagens/originais"
+destino = "imagens/geradas"
+caminho = [os.path.join(projeto, f) for f in os.listdir(projeto)]
 
-# noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
- 
-#thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+def salvar(img):
+  fig = plt.gcf() # busca a figura atual
+  fig.set_size_inches(20, 10) #define o tamanho
+  plt.axis("off") #remove a visualização dos eixos
+  plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) #faz a conversão de cores com o OpenCV
+  plt.imshow(destino) # salva a imagem
 
-#dilation
-def dilate(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.dilate(image, kernel, iterations = 1)
+config_tesseract = "/usr/share/tesseract-ocr/4.00/tessdata"
+
+def OCR_processa(img, config_tesseract):
+  texto = pytesseract.image_to_string(img, lang='por', config=config_tesseract)
+  return texto
+
+
+
+texto_completo = ''
+nome_txt = 'resultados_ocr.txt'
+
+for imagem in caminho: # percorre as imagens no caminho
+  img = cv2.imread(imagem) 
+  nome_imagem = os.path.split(imagem)[-1] ## recebe os nomes e diretórios das imagens, quebrados, precisamos apenas do -1 (última posição do diretório)
+  nome_divisao = '===================\n' + str(nome_imagem) #divisão + nome da imagem que está sendo vista
+  texto_completo = texto_completo + nome_divisao + '\n' # recebe o texto completo + a divisão + /n para pular a linha
+  texto = OCR_processa(img, config_tesseract) #passa a imagem que vamos utilizar, no caso em cada imagem
+  texto_completo = texto_completo + texto # concatena as duas variáveis
+
+## Salvando o arquivo em txt
+arquivo_txt = open(nome_txt, 'w+') # a+ é para colocar no final do arquivo, w+ para sobre escrever o arquivo
+arquivo_txt.write(texto_completo + '\n') #passa o texto que quer adicionar
+arquivo_txt.close()
+
+
+
+termo_pesquisa = input('Digite a palavra que deseja pesquisar: ')
+lang = input('Digite o idioma da imagem: [EN/PT]')
+if lang == 'EN':
+  lang = 'eng'
+elif lang == 'PT':
+    lang = 'por'
+
+for imagem in caminho:
+  img = cv2.imread(imagem) #carrega a imagem
+  nome_imagem = os.path.split(imagem)[-1] # passa a imagem e acessa a última posição do diretório
+  print('====================\n' + str(nome_imagem)) # separação + nome da imagem
+
+  texto = OCR_processa(img, config_tesseract) #usando o OCR
+
+  ocorrencias = [i.start() for i in re.finditer(termo_pesquisa, texto)] #usando o finditer novamente no texto
+
+  print('Número de ocorrências para o termo: {}: {}'.format(termo_pesquisa, len(ocorrencias))) 
+  #primeira chaves é para termo de pesquisa e a segunda é para ocorrencias
+  print('\n')
+
+
+fonte_dir = 'calibri.ttf'
+
+def escreve_texto(texto, x, y, img, fonte_dir, cor=(50, 50, 255), tamanho=16):
+  fonte = ImageFont.truetype(fonte_dir, tamanho)
+  img_pil = Image.fromarray(img) 
+  draw = ImageDraw.Draw(img_pil) 
+  draw.text((x, y-tamanho), texto, font = fonte, fill = cor) 
+  img = np.array(img_pil) 
+
+  return img
+
+min_conf = 30
+
+def caixa_texto(i, resultado, img, cor=(255, 100, 0)):
+  x = resultado["left"][i]
+  y = resultado["top"][i]
+  w = resultado["width"][i]
+  h = resultado["height"][i]
     
-#erosion
-def erode(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.erode(image, kernel, iterations = 1)
+  cv2.rectangle(img, (x, y), (x + w, y + h), cor, 2)
 
-#opening - erosion followed by dilation
-def opening(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+  return x, y, img
 
-#canny edge detection
-def canny(image):
-    return cv2.Canny(image, 100, 200)
+def OCR_processa_imagem(img, termo_pesquisa, config_tesseract, min_conf):
+  resultado = pytesseract.image_to_data(img, config=config_tesseract, lang='por', output_type=Output.DICT) #imagem para dados, que já fizemos anteriormente
+  num_ocorrencias = 0 #inicializando como 0
 
-#skew correction
-def deskew(image):
-    coords = np.column_stack(np.where(image > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-     if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated
+  for i in range(0, len(resultado['text'])): # vai de 0 ao tamanho do número de valores do texto
+    confianca = int(resultado['conf'][i]) # qual a confiança da detecção
+    if confianca > min_conf: # se a confiança for maior que o valor mínimo, passa para a linha abaixo
+      texto = resultado['text'][i] #texto será igual ao resultado text no momento i 
+      if termo_pesquisa in texto: #se o termo de pesquisa estiver no texto:
+        x, y, img = caixa_texto(i, resultado, img, (0,0,255)) # faz a caixa de bounding box
+        img = escreve_texto(texto, x, y, img, fonte_dir, (50,50,225), 14) #escreve o texto 
+        
+        num_ocorrencias += 1 #faz a iteração no num de ocorrências e volta para o laço até acabar o texto
+  return img, num_ocorrencias
 
-#template matching
-def match_template(image, template):
-    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+
+for imagem in caminho:
+  img = cv2.imread(imagem) #carrega a imagem
+  img_original = img.copy() #cria uma cópia da imagem para não escrever em cima da original
+
+  nome_imagem = os.path.split(imagem)[-1] # passa a imagem e acessa a última posição do diretório
+  print('===================\n' + str(nome_imagem))  # separação + nome da imagem
+
+  img, numero_ocorrencias = OCR_processa_imagem(img, termo_pesquisa, config_tesseract, min_conf) #usando o OCR_processa_imagem
+  print('Número de ocorrências para {} em {}: {}'.format(termo_pesquisa, nome_imagem, numero_ocorrencias))
+    #primeira chaves é para termo de pesquisa e a segunda nome das imagens e a terceira é o numeros de ocorrencias
+  print('\n')
+
+  salvar(img)
+
